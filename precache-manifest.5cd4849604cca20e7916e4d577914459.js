e8b863613bfdbcde7e0c0124d33a4ae5f86bef42
OlegKhodakivskyi <oleghodakovsky@gmail.com>
2020-09-08 18:04:39 +0300

    Updates

---
+++ precache-manifest.5cd4849604cca20e7916e4d577914459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df184503871564e5026e125c3ed5b031",
+    "url": "/goit-react-hw-05-phonebook-animation/index.html"
+  },
+  {
+    "revision": "851113d15c08d33d3ac0",
+    "url": "/goit-react-hw-05-phonebook-animation/static/css/main.44c98311.chunk.css"
+  },
+  {
+    "revision": "c5629a2643d692183a18",
+    "url": "/goit-react-hw-05-phonebook-animation/static/js/2.dfb33707.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook-animation/static/js/2.dfb33707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "851113d15c08d33d3ac0",
+    "url": "/goit-react-hw-05-phonebook-animation/static/js/main.7c1ce03a.chunk.js"
+  },
+  {
+    "revision": "222eea967fe28394189d",
+    "url": "/goit-react-hw-05-phonebook-animation/static/js/runtime-main.b990cd63.js"
+  }
+]);
